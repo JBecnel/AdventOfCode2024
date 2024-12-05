bcfd4da026d4e738308c270c891fc62b3bbe15a7
J Becnel <16531705+JBecnel@users.noreply.github.com>
2024-12-05 19:12:28 +0000

    problem 3 part 1

---
+++ problem3.py
@@ -0,0 +1,20 @@
+import re
+
+file_name = "puzzle3_prob.dat"
+#file_name = 'puzzle3_sample.dat'
+
+# regex pattern for mul(<int>,<int>)
+pattern = "mul\([0-9]+,[0-9]+\)"
+
+with open(file_name, 'r') as puzzle_file:
+    sum = 0  # tracks thue sum
+    for line in puzzle_file:
+        matches = re.findall(pattern, line)  # find all the multiple instructions
+        print(matches)      
+        for match in matches: # pull the numbers out, multiply then and add to the sum
+            nums = re.findall(r"\d+", match)
+            sum = sum + int(nums[0]) * int(nums[1])
+
+print(sum)
+  
+       
