0b9219e9d89ae9f52ef3151ad0fb92d34db9bd4e
J Becnel <16531705+JBecnel@users.noreply.github.com>
2024-12-18 00:41:05 +0000

    problem17 part 2

--- problem17_part2.py
+++ problem17_part2.py
@@ -20,10 +20,55 @@ def read_puzzle_input(filename):
     
     return registers, program
 
-# applies the op code to the operand and updates the
-# values in registers
-# instruction location
-# output of program
+# Here is the breakdown of what's happening.
+# Notice that we only process one octal digit at a time.
+# A = [x1,x2,...., xn] in oct
+# 2,4
+# B = xn  (B is 0,1,2,3,4,5,6,7)
+# 1,3
+# B = xn XOR 011   (B is 4, 2, 1, 0, 7, 6, 5, 4)
+# 7, 5
+# C  = A >> B 
+# 0, 3 
+# A = [x1,x2,....,xn-1] (lose digit) 
+# 4,1
+# B = B XOR C  ???
+# 1,5
+# B = B XOR 101 
+# 5, 5
+# write B
+# 3, 0
+# go to start
+
+def invert(program, reg):
+        # perform a BSF to find hte solutions
+        queue = [(15, 0)] # number of instructions to consider, starting value for a
+                          # start with only the last instruction and work backwards
+        answers = [] # potential values for the A that lead to the desired output (there should be 8)
+        while queue:  # while the queue is not eimpty
+            i, A_val = queue.pop(0)
+            if i >= 0:
+                for oct in range(8):  # possible next octal digit
+                    # shift the current value of a over 3 to make room for next oct
+                    next_A = (A_val << 3) + oct
+                   
+                    # get the output from the program given the 
+                    # program instructions (program)
+                    # starting value for a
+                    output = run_program(reg, program, next_A)
+                    if output == program[i:]: # if the output matches the program instructions (starting at i)
+                        if i == 0:
+                            answers.append(next_A)
+                        queue.append((i-1, next_A))
+
+        print("answer", min(answers)) # take the min of the 8 answers
+
+# process the given instruction
+# opcode - operations to conduct
+# operand - designates values to apply it to
+# registers - values of the registry
+# instruction - index of current instruction
+# output - current output
 def process_instruction(opcode, operand, registers, instruction, output):
     combo_op = get_combo_operand(operand, registers)
     if opcode == 0:
@@ -59,7 +104,7 @@ def process_instruction(opcode, operand, registers, instruction, output):
         # cdv : 
         num = registers['A']     
         # cdv: dividing by 2^operand shifts the bits to the right        
-        # stores the result in B
+        # stores the result in c
         registers['C'] = num >> combo_op
         
     return instruction+ 2
@@ -78,16 +123,6 @@ def get_combo_operand(operand, registers):
     
      return -1 # invalid operand
 
-# interprets the given list as a base 8 number with
-# 8 spot, 64 spot, 512 spot, etc
-def base_8(program):
-    place = 8
-    num = 0
-    for p in program:
-        num = num + place*p
-        place = place * 8
-    return num
-
 
 def run_program(registers, program, value):
     output = []
@@ -98,34 +133,21 @@ def run_program(registers, program, value):
 
     instruction_index = 0
     while instruction_index < len(range(len(program))):
+        # grabe the op code and operand
         opcode = program[instruction_index]
         operand = program[instruction_index + 1]
-    
-        
+        # process the instruction and return the new index
         instruction_index =  process_instruction(opcode, operand, registers, instruction_index, output)
-
-        if len(output) > 0:
-            if output != program[0:len(output)]:
-                return []
-
+    # output of program
     return output
     
-
 def main():
     # Read the input file
     file_name = 'puzzle17_sample1.dat'
     file_name = 'puzzle17.dat'
     registers, program = read_puzzle_input(file_name)
 
-      # on viewing the debug file, we realize
-    # that we are printing in the numbers in base 8, 
-    # so we need the number in base 8
-    # that matches the program input
-
-    # interpret the program as a number in base 8
-    # puzzle solution can be any of num, num+1, ...., num+7
-    num = base_8(program)
-    print(num)
+    invert(program, registers)
 
     if dBug:
         debug_file = open('debug.txt', 'w')
@@ -136,23 +158,23 @@ def main():
    
     # find the one that gives us the output
     output = []
-    
+    # displays some values for pattern recognition    
+    if dBug:
+        for reg_A in range(pow(8,len(program)), pow(8, len(program)+1) ):
+        #for reg_A in range(num, num+1):
+            value = reg_A    
+            #if dBug:
+            #value = pow(8, reg_A)
 
-    for reg_A in range(pow(8,len(program)), pow(8, len(program)+1) ):
-    #for reg_A in range(num, num+1):
-        value = reg_A    
-        #if dBug:
-           #value = pow(8, reg_A)
+            output = run_program(registers,program, value)
+            if dBug and len(output) > 0:
+                print("A", reg_A, file=debug_file)
+                print("output", output, file=debug_file)
 
-        output = run_program(registers,program, value)
-        if dBug and len(output) > 0:
-            print("A", reg_A, file=debug_file)
-            print("output", output, file=debug_file)
-
-        
-        if output == program:
-            print(reg_A)
-                
+            
+            if output == program:
+                print(reg_A)
+                    
 
 
 
