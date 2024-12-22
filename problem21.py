371742ced82e19a4a2388c5c7527b58da8f155b9
JBecnel <becnel@gmail.com>
2024-12-22 14:10:15 -0600

    problem21

--- problem21.py
+++ problem21.py
@@ -3,15 +3,8 @@ import networkx as nx
 from enum import Enum
 from collections import deque
 
-dBug = True
+# failed attempt
 
-
-class Buttons(Enum):
-    UP = '^'
-    LEFT = '<'
-    DOWN = 'v'
-    RIGHT = '>'
-    ACTIVATE = 'A'
 
 def obtain_input(filename):
     with open(filename, 'r') as file:
@@ -30,75 +23,12 @@ def obtain_input(filename):
 # +---+---+---+
 #     | 0 | A |
 #     +---+---+
-def key_pad_graph():
-    G = nx.DiGraph()
-    G.add_edge('A', '0', label=Buttons.LEFT) 
-    G.add_edge('A', '3', label=Buttons.UP)
-    
-    G.add_edge('0','A', label=Buttons.RIGHT) 
-    G.add_edge('0', '2', label=Buttons.UP)
-
-    G.add_edge('1', '2', label=Buttons.RIGHT)
-    G.add_edge('1', '4', label=Buttons.UP)
-
-    G.add_edge('2', '1', label=Buttons.LEFT)
-    G.add_edge('2', '0', label=Buttons.DOWN)
-    G.add_edge('2', '3', label=Buttons.RIGHT)
-    G.add_edge('2', '5', label=Buttons.UP)
-
-    G.add_edge('3', '2', label=Buttons.LEFT)
-    G.add_edge('3', 'A', label=Buttons.DOWN)
-    G.add_edge('3', '6', label=Buttons.UP)
-
-    G.add_edge('4', '1', label=Buttons.DOWN)
-    G.add_edge('4', '5', label=Buttons.RIGHT)
-    G.add_edge('4', '7', label=Buttons.UP)
-
-    G.add_edge('5', '2', label=Buttons.DOWN)
-    G.add_edge('5', '4', label=Buttons.LEFT)
-    G.add_edge('5', '6', label=Buttons.RIGHT)
-    G.add_edge('5', '8', label=Buttons.UP)
-
-    G.add_edge('6', '3', label=Buttons.DOWN)
-    G.add_edge('6', '5', label=Buttons.LEFT)
-    G.add_edge('6', '9', label=Buttons.UP)
-
-    G.add_edge('7', '4', label=Buttons.DOWN)
-    G.add_edge('7', '8', label=Buttons.RIGHT)
-
-    G.add_edge('8', '5', label=Buttons.DOWN)
-    G.add_edge('8', '7', label=Buttons.LEFT)
-    G.add_edge('8', '9', label=Buttons.RIGHT)
-
-    G.add_edge('9', '6', label=Buttons.DOWN)
-    G.add_edge('9', '8', label=Buttons.LEFT)
-
-    return G
-    
     
 #     +---+---+
 #     | ^ | A |
 # +---+---+---+
 # | < | v | > |
 # +---+---+---+   
-def direction_pad_graph():
-    G = nx.DiGraph()
-    G.add_edge('A', '^', label=Buttons.LEFT)
-    G.add_edge('A', '>', label=Buttons.DOWN)
-    
-    G.add_edge('^', 'A', label=Buttons.RIGHT)
-    G.add_edge('^', 'v', label=Buttons.DOWN)
-
-    G.add_edge('<', 'v', label=Buttons.RIGHT)
-    
-    G.add_edge('v', '>', label=Buttons.RIGHT)
-    G.add_edge('v', '<', label=Buttons.LEFT)
-    G.add_edge('v', '^', label=Buttons.UP)
-
-    G.add_edge('>', 'v', label=Buttons.LEFT)
-    G.add_edge('>', 'A', label=Buttons.UP)
-
-    return G
 
 # +---+---+---+
 # | 7 | 8 | 9 |
@@ -109,107 +39,199 @@ def direction_pad_graph():
 # +---+---+---+
 #     | 0 | A |
 #     +---+---+
-def get_paths_for_keys(key_graph):
-    keys = ['A', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    key_paths = { }
-    for k1 in keys:
-        for k2 in keys:
-            if k1 != k2:
-                paths = nx.all_shortest_paths(key_graph, k1, k2)
-                path_list = [ ]
-                for p in paths:
-                    path = p
-                    path_str = ""
-                    for k in range(len(path)-1):
-                        path_str += key_graph[path[k]][path[k+1]]['label'].value
-                    path_list.append(path_str)
-                key_paths[(k1,k2)] = path_list
-                
-                
-    return key_paths
-
-def get_paths_for_dirs(dir_graph):
-    keys = ['A', '>', '^', 'v', '<']
-    key_paths = { }
-    for k1 in keys:
-        for k2 in keys:
-            paths = nx.all_shortest_paths(dir_graph, k1, k2)
-            path_list = [ ]
-            for p in paths:
-                path = p
-                path_str = ""
-                for k in range(len(path)-1):
-                    path_str += dir_graph[path[k]][path[k+1]]['label'].value
-                path_list.append(path_str)
-            key_paths[(k1,k2)] = path_list
-            
-                
-                
-    return key_paths
-
-
-
-def weight_dict_last(graph):
-    weight = { }
-    for b1 in Buttons:
-        for b2 in Buttons:
-            if b1 != b2:
-                path = nx.shortest_path(graph, b1.value, b2.value)
-                weight[(b1.value, b2.value)] = len(path)
+def get_paths_for_keys():
+    keys = [     'A',    '0', '1',     '2',   '3',   '4', '5',     '6',   '7', '8', '9']
+    locations = [(2,3), (1,3), (0,2), (1,2), (2,2), (0,1), (1,1), (2,1), (0,0), (1,0), (2,0)]
+    key_path = { }
+    for i in range(len(keys)):
+        for j in range(len(keys)):
+            key1 = keys[i]
+            key2 = keys[j]
+            if (i == j):
+                key_path[(key1,key2)] = "A"
             else:
-                weight[(b1.value, b2.value)] = 1
+                str = ""
+                # key2 - key1 
+                hor = locations[j][0]- locations[i][0]
+                ver = locations[j][1] - locations[i][1]
+                # go left, then up/down, then right except if you are going to hit blank
+                
+                # get left 
+                if hor < 0:
+                    str = "<" * abs(hor)
+                if ver < 0:
+                    str += "^" * abs(ver)
+                else:
+                    str += "v" * ver
+                    
+                if hor > 0:
+                    str += ">" * hor
+                
+                key_path[(key1,key2)] = str + "A"
+                
+    # fix the incorrect ones
+    key_path[('A', '1')] = "^<<A"
+    key_path[('A', '4')] = "^^<<A"
+    key_path[('A', '7')] = "^^^<<A"
+    key_path[('1', 'A')] = ">>vA"
+    key_path[('4', 'A')] = ">>vvA"
+    key_path[('7', 'A')] = ">>vvvA"
+    
+    key_path[('0', '1')] = "^<A"
+    key_path[('0', '4')] = "^^<A"
+    key_path[('0', '7')] = "^^^<A"
+    key_path[('1', '0')] = ">vA"
+    key_path[('4', '0')] = ">vvA"
+    key_path[('7', '0')] = ">vvvA"
+    
+    
+    return key_path
 
-    return weight
+
+# +---+---+---+
+# | 7 | 8 | 9 |
+# +---+---+---+
+# | 4 | 5 | 6 |
+# +---+---+---+
+# | 1 | 2 | 3 |
+# +---+---+---+
+#     | 0 | A |
+#     +---+---+
+
+                    
+  #  379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+#     +---+---+
+#     | ^ | A |
+# +---+---+---+
+# | < | v | > |
+# +---+---+---+   
+def get_paths_for_dirs():
+    keys = ['A', '>', '^', 'v', '<']
+    locations = [(2,0), (2,1), (1,0), (1,1), (0,1)]
+    
+    # go left, then up/dow, then rigth (unless a blank)
+    key_path = { }
+    for i in range(len(keys)):
+        for j in range(len(keys)):
+            key1 = keys[i]
+            key2 = keys[j]
+            if (i == j):
+                key_path[(key1,key2)] = "A"
+            else:
+                str = ""
+                # key2 - key1 
+                hor = locations[j][0]- locations[i][0]
+                ver = locations[j][1] - locations[i][1]
+                # go left, then up/down, then right except if you are going to hit blank
+                
+                # get left 
+                if hor < 0:
+                    str += "<" * abs(hor)
+                if ver < 0:
+                    str += "^" * abs(ver)
+                else:
+                    str += "v" * ver    
+                if hor > 0:
+                    str += ">" * hor
+                 
+                key_path[(key1,key2)] = str + "A"
+                
+    # fix the two incorrect ones
+    key_path[('A', '<')] = "v<<A"
+    key_path[('^', '<')] = "v<A"
+    key_path[('<', 'A')] = ">>^A"
+    key_path[('<', '^')] = ">^A"
+    return key_path
+                
+    
+#     +---+---+
+#     | ^ | A |
+# +---+---+---+
+# | < | v | > |
+# +---+---+---+   
+# <vA<AA>>^A vAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+# v<<A >>^A<A>AvA<^AA>A<vAAA>^A
+# A<A^A>^^AvvvA                
+   
 
 
-
-def generate_first_strings(code, num_paths):
+def parse_code(code,num_paths):
     code = "A" + code
-    all_paths = [ "" ]
+    str = ""
     for i in range(len(code)-1):
-        key = (code[i], code[i+1])
-        all_paths = [ a + b + "A" for a in all_paths for b in num_paths[key]]
+        key1 = code[i]
+        key2 = code[i+1]
+        str += num_paths[(key1,key2)]
+        
+    return str 
 
-    return all_paths
+def get_dir_path_length(dir_paths):
+    length = { }
+    for key in dir_paths:
+        path = dir_paths[key]
+        length[key] = len(path)
+    return length
 
-def generate_second_strings(str, dir_paths):
-    str = "A" + str
-    all_paths = [ "" ]
-    for i in range(len(str)-1):
-        key = (str[i], str[i+1])
-        all_paths = [ a + b + "A" for a in all_paths for b in dir_paths[key]]
+def reverse(dir_length, dir_paths):
+    new_lengths = { }
+    for key in dir_length:
+        path =  dir_paths[key]
+        sum = 0
+        for i in range(len(path)-1):
+            key1 = path[i]
+            key2 = path[i+1]
+            sum =  sum + dir_length[(key1,key2)]
+        if key[0] != key[1]:
+            new_lengths[key] = sum
+        else:
+            new_lengths[key] = 1
+    return new_lengths
 
-    return all_paths
-
-def find_best(second_possible, last_key_pad):
-    best = sys.maxsize
-    for s in second_possible:
-        s = "A" + s
-        total = 0
-        for i in range(len(s)-1):
-            total += last_key_pad[(s[i], s[i+1])]
-        if total < best:
-            best = total
-
-    return best
+def code_length(dir_length, code):
+    sum = 0
+    code = "A" + code
+    for i in range(len(code)-1):
+        key1 = code[i]
+        key2 = code[i+1]
+        sum =  sum + dir_length[(key1,key2)]
+    return sum
 
 def main():
     file_name = "puzzle21_sample.dat"
     file_name = "puzzle21.dat"
     codes = obtain_input(file_name)
 
-    num_pad_G = key_pad_graph()
-    dir_pad_G = direction_pad_graph()
-    last_key_pad = weight_dict_last(dir_pad_G)
     
-    print(last_key_pad)
-
-    num_paths = get_paths_for_keys(num_pad_G)
-    #print(num_paths) 
-
-
-    dir_paths = get_paths_for_dirs(dir_pad_G)
+    num_paths = get_paths_for_keys()
+    #print(num_paths)
+    print()
+    dir_paths = get_paths_for_dirs()
+    
+    # create start path length
+    dir_length = get_dir_path_length(dir_paths)
     print(dir_paths)
+    print()
+    print(dir_length) 
+
+    for i in range(2):
+        dir_length = reverse(dir_length, dir_paths)
+        print(dir_length)
+        print()
+        
+    print()
+    lengths = [ ]
+    for code in codes:
+        #print(code)
+        start = parse_code(code,  num_paths)
+        print(start)
+        length = code_length(dir_length, start)
+        #for i in range(25):
+            #print(start)
+            #start = parse_code(start, dir_paths)
+        #print(start)
+        lengths.append(length)
+        print(length)
+   
     
     with open(file_name, 'r') as file:
         numbers = [int(line.strip()[:-1].lstrip('0')) for line in file if line.strip().endswith('A')]
@@ -217,18 +239,7 @@ def main():
 
     answer  = 0
     for i in range(len(codes)):
-        code = codes[i]
-        first_possibles = generate_first_strings(code, num_paths)
-        #print(first_possibles)
-
-        second_possible = [ ]
-        for str in first_possibles:
-            second_possible += generate_second_strings(str, dir_paths)
-
-        #print(second_possible)
-
-        length = find_best(second_possible, last_key_pad)
-        answer += length *  numbers[i]
+        answer += lengths[i] * numbers[i]
 
     print(answer)
 
